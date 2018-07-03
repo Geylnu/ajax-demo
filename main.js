26f9e4a708758631b36728ae9c1a4ef281b9f48b
geylnu <yanglei1997630@gmail.com>
2018-07-03 15:26:54 +0800

    使用Promise进行回调

--- main.js
+++ main.js
@@ -3,33 +3,34 @@ window.jQuery = function (){
 }
 
 window.$=window.jQuery
-window.jQuery.ajax = function ({url,method,headers,body,successFn,failFn}){
-    let request = new XMLHttpRequest()
-    request.open(method,url)
-    for(let key in headers){
-        request.setRequestHeader(key,headers[key])
-    }
-    request.onreadystatechange = (e)=>{
-        if(request.readyState === 4){
-            if(request.status <=300 && request.status>=200){
-                successFn.call(undefined,request.responseText)
-            }else if(request.status >=400){ 
-                failFn.call(undefined,request)
+window.jQuery.ajax = function ({url,method,headers,body}){
+    return new Promise(function (resolve,reject){
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        for(let key in headers){
+            request.setRequestHeader(key,headers[key])
+        }
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status <=300 && request.status>=200){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >=400){ 
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-
-    request.send(body)
+    
+        request.send(body)
+    })
 }
+
 test.addEventListener('click',(e)=>{
     window.$.ajax({url:'/test',
-     method: 'post',
-     body: 'name=111',
-     successFn: ()=>{console.log('success')},
-     failFn: ()=>{console.log('fail')},
-     headers: {
-         myOwnHeader:'666',
-         'Content-type':'application/x-www-form-urlencoded'
+    method: 'post',
+    body: 'name=111',
+    headers: {
+        myOwnHeader:'666',
+        'Content-type':'application/x-www-form-urlencoded'
         }
-    })
+    }).then(()=>{console.log('success')},()=>{console.log('fail')})
 })
